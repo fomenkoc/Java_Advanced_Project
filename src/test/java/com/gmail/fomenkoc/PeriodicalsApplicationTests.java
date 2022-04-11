c51d8bd19801e361b5a179d5bcd3947c04ca1d56
Alexander Fomenko <fomenkoc@gmail.com>
2022-04-11 17:18:12 +0300

    init

---
+++ src/test/java/com/gmail/fomenkoc/PeriodicalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.fomenkoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeriodicalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
