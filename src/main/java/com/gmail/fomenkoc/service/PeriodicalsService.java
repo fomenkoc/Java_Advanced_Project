6a4f0b0cfb35a6a0548bd984cf0e7e4a7069b25b
Alexander Fomenko <fomenkoc@gmail.com>
2022-05-03 16:52:24 +0300

    log in service

--- src/main/java/com/gmail/fomenkoc/service/PeriodicalsService.java
+++ src/main/java/com/gmail/fomenkoc/service/PeriodicalsService.java
@@ -2,6 +2,8 @@ package com.gmail.fomenkoc.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,20 +12,24 @@ import com.gmail.fomenkoc.domain.Periodical;
 
 @Service
 public class PeriodicalsService {
-	
+	private Logger logger = LoggerFactory.getLogger(PeriodicalsService.class);
+
 	@Autowired
 	private PeriodicalRepository periodicalRepository;
-	
+
 	public Periodical save(Periodical periodical) {
+		logger.info("Create new periodical {}: " + periodical);
 		return periodicalRepository.save(periodical);
 	}
-	
-	public List<Periodical> getAllPeriodicals(){
+
+	public List<Periodical> getAllPeriodicals() {
+		logger.info("Get all periodicals");
 		return periodicalRepository.findAll();
 	}
-	
+
 	public Periodical findById(Integer id) {
+		logger.info("Create new periodical by id: " + id);
 		return periodicalRepository.findById(id).get();
 	}
-	
+
 }
--- src/main/java/com/gmail/fomenkoc/service/UserService.java
+++ src/main/java/com/gmail/fomenkoc/service/UserService.java
@@ -1,5 +1,7 @@
 package com.gmail.fomenkoc.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -9,22 +11,25 @@ import com.gmail.fomenkoc.domain.User;
 import com.gmail.fomenkoc.domain.UserRole;
 
 @Service
-public class UserService{
-    @Autowired
-    private UserRepository userRepository;
+public class UserService {
+	private Logger logger = LoggerFactory.getLogger(UserService.class);
 
-    @Autowired
-    private PasswordEncoder bCryptPasswordEncoder;
+	@Autowired
+	private UserRepository userRepository;
 
+	@Autowired
+	private PasswordEncoder bCryptPasswordEncoder;
 
-    public void save(User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setRole(UserRole.ROLE_USER);
-        userRepository.save(user);
-    }
-    
-    public User findByEmail(String email) {
-    	return userRepository.findByEmail(email).get();
-    }
+	public void save(User user) {
+		logger.info("Create new user {}: " + user);
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		user.setRole(UserRole.ROLE_USER);
+		userRepository.save(user);
+	}
+
+	public User findByEmail(String email) {
+		logger.info("Get user by email: " + email);
+		return userRepository.findByEmail(email).get();
+	}
 
 }
